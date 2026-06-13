7d87d1b6aac71f54fb1aa4bfceea515caac0f358
ZarifAhmed997 <zarifinzarif@gmail.com>
2026-06-13 18:16:48 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+GMAIL_ADRESS = "zarifinzarif@gmail.com"
+GMAIL_PASSWORD = ""
+NOTIFY_EMAILS = ["your_email@example.com"]
+
+# Replace with your actual search URLs
+SEARCH_URLS = ["https://www.example.com/search"]
+
+MAX_PRICE = 760 # GBP
+MIN_BEDS = 3
+MOVE_IN_DATE = "2024-07-01"  # Format: YYYY-MM-DD
+
+"""
+Include check for HMO
+Include Location filter
+Include move in date filter
+"""
+
+INTERVAL = 15 # minutes
